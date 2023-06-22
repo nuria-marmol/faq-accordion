c7ec268e99e8ba4f77cd9d8a11e4d577c2c3615e
nuria-marmol <numarbru@gmail.com>
2023-06-22 13:27:01 +0200

    code review

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 const articles = document.querySelectorAll("article");
 
 /**
- * Concrete (and dynamic) article height according to the title, in case a long 
- * question is added
+ * Concrete (and dynamic) article height according to the title group
  */
 function definingArticleHeight() {
     articles.forEach(function (article) {
-        const title = article.querySelector(".faq__question-block");
-        article.style.height = `${title.offsetHeight}px`;
+        const questionBlock = article.querySelector(".faq__question-block");
+        article.style.height = `${questionBlock.offsetHeight}px`;
     })
 }
 
@@ -17,16 +16,14 @@ articles.forEach(function (article) {
         const title = article.querySelector("h2");
         const arrow = article.querySelector("img");
         const answer = article.querySelector(".faq__answer");
-        // Adding answer height to the article
-        if (!answer.classList.contains("show")) { 
-            title.style.fontSize = "1rem";
+        /* Adding answer height to the article and other styles to the arrow and title */
+        if (!answer.classList.contains("show")) {            
             title.style.fontWeight = "700";
-            arrow.classList.add("turn");         
+            arrow.classList.add("turn");     
             answer.classList.add("show");
             article.style.height = `${article.offsetHeight + answer.offsetHeight}px`;
-        // Article height back to previous style if the user wants to hide the answer
-        } else {
-            title.style.fontSize = ".9rem";
+        /* Article height and other styles back to previous state if the user wants to hide the answer */
+        } else {            
             title.style.fontWeight = "400";
             arrow.classList.remove("turn");
             answer.classList.remove("show");     
