9a7f978a88d8d67a1f4833f93ce5fb6ac8e3106b
nuria-marmol <numarbru@gmail.com>
2023-06-23 14:39:40 +0200

    fixed height bug

--- js/main.js
+++ js/main.js
@@ -1,35 +1,27 @@
 const articles = document.querySelectorAll("article");
 
-/**
- * Concrete (and dynamic) article height according to the title group
- */
-function definingArticleHeight() {
-    articles.forEach(function (article) {
-        const questionBlock = article.querySelector(".faq__question-block");
-        article.style.height = `${questionBlock.offsetHeight}px`;
-    })
-}
-
 // Event
 articles.forEach(function (article) {
     article.addEventListener("click", function() {
+        const questionBlock = article.querySelector(".faq__question-block");
         const title = article.querySelector("h2");
         const arrow = article.querySelector("img");
         const answer = article.querySelector(".faq__answer");
         /* Adding answer height to the article and other styles to the arrow and title */
-        if (!answer.classList.contains("show")) {            
+        if (!answer.classList.contains("show")) {
+            // We need a concrete size as an initial state
+            article.style.height = `${questionBlock.offsetHeight}px`;        
             title.style.fontWeight = "700";
-            arrow.classList.add("turn");     
+            arrow.classList.add("turn"); 
             answer.classList.add("show");
-            article.style.height = `${article.offsetHeight + answer.offsetHeight}px`;
+            // 12 for some space at the bottom
+            article.style.height = `${questionBlock.offsetHeight + answer.offsetHeight + 12}px`
         /* Article height and other styles back to previous state if the user wants to hide the answer */
         } else {            
             title.style.fontWeight = "400";
             arrow.classList.remove("turn");
-            answer.classList.remove("show");     
-            article.style.height = `${article.offsetHeight - answer.offsetHeight}px`;                      
+            answer.classList.remove("show");
+            article.style.height = `${questionBlock.offsetHeight}px`;                      
         }  
     });
 })
-
-definingArticleHeight();
